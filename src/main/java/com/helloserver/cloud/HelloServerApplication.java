3c41ceca260ebd69692b5a9464fb4b5a98a803fd
aparna <aparna.06cs03@gmail.com>
2022-08-23 22:35:43 +0530

    changes

--- src/main/java/com/helloserver/cloud/HelloServerApplication.java
+++ src/main/java/com/helloserver/cloud/HelloServerApplication.java
@@ -2,9 +2,8 @@ package com.helloserver.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+
 @SpringBootApplication
 public class HelloServerApplication {
 
